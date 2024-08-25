b3cfa7e16c12c11432c1fc1bd3b9cd124a672193
Calixxx <calixmonnet@gmail.com>
2024-08-25 18:45:16 +0000

    writing start

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "http://localhost:4321/",
   integrations: [
+    mdx({}),
     icon({
       iconDir: "src/icons",
       include: {
