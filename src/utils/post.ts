ae2bf5be0ff7a4189151ae9f33672f40ab226ec5
Calixx <77709844+calixmnt@users.noreply.github.com>
2026-01-03 10:06:00 +0000

    layout

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,5 +1,13 @@
 import { type CollectionEntry, getCollection } from "astro:content";
 
 export const getAllPosts = async (): Promise<CollectionEntry<"post">[]> => {
-  return await getCollection("post");
+  const posts = await getCollection("post", ({ data }) => data.draft !== true);
+
+  return posts.filter(
+    (p): p is CollectionEntry<"post"> =>
+      !!p &&
+      !!p.data &&
+      !!p.data.title &&
+      !!p.data.publishDate
+  );
 };
